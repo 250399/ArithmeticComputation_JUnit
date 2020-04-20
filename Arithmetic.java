ae9d605b6a4f2f20bab35b5fe715821d4e59183b
250399 <kunaldeshmukh2503@gmail.com>
2020-04-20 06:41:19 -0700

    ADDED: Storing results in Hash Map

--- Arithmetic.java
+++ Arithmetic.java
@@ -1,26 +1,34 @@
 package com.bl.demo;
 
+import java.util.HashMap;
+
 public class Arithmetic {
     int a;
     int b;
     int c;
+    HashMap<String,Integer> computationResults = new HashMap<>();
     Arithmetic(int a, int b, int c){
         this.a=a;
         this.b=b;
         this.c=c;
     }
     int computeFirstExpression(){
+        computationResults.put("result1",(a+b*c));
         return (a+b*c);
     }
 
     int computeSecondExpression(){
+        computationResults.put("result2",(a*b+c));
         return (a*b+c);
     }
     int computeThirdExpression() {
+        computationResults.put("result3",(c+a/b));
         return (c+a/b);
     }
     int computeFourthExpression(){
+        computationResults.put("result4",(a%b+c));
         return (a%b+c);
     }
+
 
 }
